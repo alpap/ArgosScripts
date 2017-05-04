8a52bbd66088b9351352e67726570ec0563ba978
alpap <al.pap@live.com>
2017-05-04 15:12:43 +0200

    initial scripts

---
+++ Ping.c4.2s.py
@@ -0,0 +1,17 @@
+import pyping
+
+r = pyping.ping('google.com')
+
+print(r)
+
+# def show(cpu):
+#     color = '#cc575d' if cpu > 75 else '#d19a66' if cpu > 50 else '#fafafa'
+#     return "<span color='%s'>%3d%%</span>" % (color, cpu)
+
+# print("%s|font=monospace" % r)
+# print("---")
+# for i, cpu in enumerate(cpus):
+#     print("CPU %d: %s|font=monospace" % (i, show(cpu)))
+# print("---")
+# print("System monitor|iconName=utilities-system-monitor-symbolic" +
+#       " bash=gnome-system-monitor terminal=false")
